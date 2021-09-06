c2a52fa1511c644c8c3c5c6204ffd5adc1a631a9
Wyatt Melin <wyattmelon@gmail.com>
2021-09-06 08:19:20 -0500

    Update example.py

--- example.py
+++ example.py
@@ -7,8 +7,6 @@ def tokens_lowercase(doc):
     '''Place your code here'''
  
     tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
-    print("Printing TOK!!")
-    print(tok)
     tok = metapy.analyzers.LowercaseFilter(tok)
     tok = metapy.analyzers.LengthFilter(tok, min=2, max=5)
     #tok.set_content(doc.content())
